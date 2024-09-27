132588ae39a9ef98d2c142428f4145edb14f64e9
Abdul Rehman <syedabdulrehman.08@gmail.com>
2024-09-27 15:46:21 +0500

    Whisper.ai

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import VideoUploader from '../../components/VideoUploader';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <VideoUploader />
+    </div>
+  );
+};
+
+export default Home;
